46fd4dfb2d7661bcef55c7b67e503d253dcf0f85
PatreIsreal <1507536812@qq.com>
2025-03-05 00:18:24 +0800

    前九道题

--- 卡码网python基础课/test.py
+++ 卡码网python基础课/test.py
@@ -1,10 +1,17 @@
 while True:
     try:
+        # 获取用户输入的正方形边长
         n = int(input())
-        for i in range(n):
-            line = list(map(int,input().split()))
-        print(line[-1:1:0])
-        print(line[0:2:-1])
-    
+
+        # 循环打印每一行
+        for row in range(n):
+            if row == 0 or row == n-1:
+                # 第一行和最后一行打印完整的“*”
+                print("*" * n)
+            else:
+                # 中间行打印“*”加空格再加“*”
+                print("*" + " " * (n-2) + "*")
     except:
         break
+
+        
