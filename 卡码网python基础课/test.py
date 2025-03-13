fc9206db260cbc14fd31ad7911933185ff920a94
YuanLiang <87492729+PatreIsreal@users.noreply.github.com>
2025-03-13 11:09:11 +0800

    卡码完结

--- 卡码网python基础课/test.py
+++ 卡码网python基础课/test.py
@@ -1,27 +1,48 @@
-# 先输入要输入字符串的行数
-n = int(input())
+# Shape类
+class Shape:
+    def __init__(self, shape_type):
+        self.type = shape_type
+     
+    def calculate_area(self):
+        pass
+#  Rectangle类，包含 width 和 height, 计算面积的方法
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__("Rectangle")
+        self.width = width
+        self.height = height
+ 
+    def calculate_area(self):
+        return self.width * self.height
+# Circle类，包含 radius, 计算面积的方法 
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__("Circle")
+        self.radius = radius
+ 
+    def calculate_area(self):
+        return 3.14 * self.radius * self.radius
+         
+shapes = []
+ 
+while True:
+    data = input().split()
+    shape_type = data[0]
+    # 处理输出
+    if shape_type == "end":
+        break
 
-# 遍历输入字符串s
-for _ in range(n):
-    s = input()
-    # 创建哈希表，这里一定要将这行代码写在for循环内部，不然会影响其他行的结果
-    # 正确的应该是每处理一行的结果再重新清空列表中的数值进行下一次的遍历
-    temp = [0] * 26
-    for char in s:
-        # 找出是哪个字母
-        number = ord(char) - ord('a')
-        # 对应的频次加一
-        temp[number] = temp[number] + 1
-
-    # 找出最高频次是多少
-    maxFreq = max(temp)
-    
-    # 找出所有出现频次等于最高频次的字母
-    max_freq_chars = []
-    for i in range(26):
-        if temp[i] == maxFreq:
-            max_freq_chars.append(chr(ord('a') + i))
-    
-    # 将所有最高频次字母连接成字符串输出
-    res = ''.join(max_freq_chars)
-    print(res)
+    if shape_type == "rectangle":
+        # 获取输入的width/height, 将之转换成整数
+        width, height = int(data[1]), int(data[2])
+        # 新建一个对象，append到列表中
+        shapes.append(Rectangle(width, height))
+    elif shape_type == "circle":
+        # 获取输入的radius, 将之转换成整数
+        radius = int(data[1])
+        # 新建一个对象，append到列表中
+        shapes.append(Circle(radius))
+ 
+for shape in shapes:
+    # 不同类别的对象调用同一个方法，有不同的处理逻辑
+    print(f"{shape.type} area: {shape.calculate_area():.2f}")
